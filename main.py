0bb96623a35961004bc92878ac0e91d7d851f045
jonhalverson9 <jonhalverson9@gmail.com>
2019-11-15 18:32:23 +0000

    Put "EnterMovies" logic into a function

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import random
 
 movie_list = []
 
+# ------------Functions------------
+def EnterMovies():
+  try:
+  
+    while True:
+      movie = input("\nEnter a Movie: ")
+      try:
+        weight = int(input("Weight of the movie? "))
+      except:
+        print("You have entered an invalid value")
+      movie_list.extend([movie] * weight)
+
+  except KeyboardInterrupt:
+    print("\n")
+    pass
+
+# ------------SCRIPT------------
 print("Press ctrl-c once finished entering movies")
 
-try:
-  
-  while True:
-    movie = input("\nEnter a Movie: ")
-    try:
-      weight = int(input("Weight of the movie? "))
-    except:
-      print("You have entered an invalid value")
-    movie_list.extend([movie] * weight)
-
-except KeyboardInterrupt:
-  print("\n")
-  pass
+EnterMovies()
 
 input("Press Enter to see results")
 print("\n--------------------------------------\n")
